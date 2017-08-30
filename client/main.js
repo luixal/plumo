661432ffaaa7fa930b5807a2a4b1cef1051103fd
Luis Alberto Pérez García <luixal@gmail.com>
2017-08-30 23:57:46 +0200

    Changes textbox for ACE code editor.

--- client/main.js
+++ client/main.js
@@ -1,14 +1,7 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-// require('../node_modules/popper.js/dist/umd/popper.min.js')
-// import Popper from 'popper.js'
-// window.Popper = Popper;
-// window.Popper = require('popper.js').default;
-import bootstrap from 'bootstrap'
 
-// import '../node_modules/jquery/dist/jquery.slim.min.js'
-// import '../node_modules/popper.js/dist/umd/popper.js'
-// import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
+import bootstrap from 'bootstrap'
 
 import './main.html';
--- client/views/editor/editor.js
+++ client/views/editor/editor.js
@@ -1,17 +1,47 @@
 import { mjml2html } from 'mjml';
 
-var mjml = new ReactiveVar();
+
+Template.editor.onCreated(function() {
+  this.mjml = new ReactiveVar();
+});
+
+Template.editor.onRendered(function() {
+  // loading ACE modules:
+  import '/node_modules/ace-builds/src-min-noconflict/ace.js';
+  import '/node_modules/ace-builds/src-min-noconflict/mode-html.js';
+  import '/node_modules/ace-builds/src-min-noconflict/theme-monokai.js';
+
+  // init editor:
+  this.editor = ace.edit('editor');
+  this.editor.getSession().setMode('ace/mode/javascript');
+  this.editor.setTheme('ace/theme/monokai');
+
+  // set mjml code in reactive var on key shortcut:
+  let mjml = this.mjml;
+  this.editor.commands.addCommand({
+    name: 'myCommand',
+    bindKey: {win: 'Ctrl-M',  mac: 'Command-M'},
+    exec: function(editor) {
+      mjml.set(editor.getValue());
+    },
+    readOnly: true // false if this command should not apply in readOnly mode
+  });
+});
 
 Template.editor.events({
   'click #preview'(event, instance) {
-    mjml.set($('#code').get(0).value);
+    // set mjml code in reactive var on button click::
+    instance.mjml.set(instance.editor.getValue());
   },
 });
 
 Template.editor.helpers({
   preview() {
-    if (mjml.get()) {
-      let cmpl = mjml2html(mjml.get());
+    // if mjml code exists, render and show it in preview box:
+    let code = Template.instance().mjml.get();
+
+    if (code) {
+      let cmpl = mjml2html(code);
       if (cmpl.errors.length) return 'Error rendering template</br>' + cmpl.errors;
       return Spacebars.SafeString(cmpl.html);
     }
